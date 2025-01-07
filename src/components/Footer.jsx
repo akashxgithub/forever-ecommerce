d968bcd34c32eff47d069c687e0e7c2aff38a1c6
akashxgithub <theog.cse.rymec@gmail.com>
2025-01-07 10:31:55 +0530

    second commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
     <div>
-      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40'>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40">
         <div>
-            <img src={assets.logo} className='mb-5 w-32' alt="" />
-            <p className='w-full md:w-2/3 text-gray-600'></p>
+          <img src={assets.logo} className="mb-5 w-32" alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">qqwwwwwwwwwwwwww </p>
+        </div>
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Provacy Policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+91 9999999999</li>
+            <li>contact@forever.com</li>
+          </ul>
         </div>
       </div>
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright 2025@forever.com - All Rights Reserved
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
