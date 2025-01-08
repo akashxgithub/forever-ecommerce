c61531ccfbfd5a5d486229696841e505195fe198
akashxgithub <theog.cse.rymec@gmail.com>
2025-01-08 23:35:12 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ import Orders from "./pages/Orders";
 import Product from "./pages/Product"; // Import the Product component
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import SearchBar from "./components/SearchBar";
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7px] lg:px-[9vw]">
       <Navbar />
+      <SearchBar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/collection" element={<Collection />} />
