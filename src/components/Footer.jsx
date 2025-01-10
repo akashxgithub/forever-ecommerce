30e15c66872fb12618524e28b6bd351898a08642
akashxgithub <theog.cse.rymec@gmail.com>
2025-01-11 00:50:08 +0530

    fourth

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -7,15 +8,15 @@ const Footer = () => {
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40">
         <div>
           <img src={assets.logo} className="mb-5 w-32" alt="" />
-          <p className="w-full md:w-2/3 text-gray-600">qqwwwwwwwwwwwwww </p>
+          <p className="w-full md:w-2/3 text-gray-600">Quirk Up Your Wardrobe</p>
         </div>
         <div>
           <p className="text-xl font-medium mb-5">COMPANY</p>
           <ul className="flex flex-col gap-1 text-gray-600">
-            <li>Home</li>
-            <li>About us</li>
+            <Link to='/'><li>Home</li></Link>
+            <Link to='/about'><li>About us</li></Link>
             <li>Delivery</li>
-            <li>Provacy Policy</li>
+            <li>Privacy Policy</li>
           </ul>
         </div>
 
